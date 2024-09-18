317db82123d2ef775cd348409fb42612a33e43e9
Pavel_Melnik <129269794+az25-boop@users.noreply.github.com>
2024-09-18 12:49:47 +0300

    api

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { fetchArticles } from "./services/api";
 
 const App = () => {
+  const [articles, setArticles] = useState([]);
   useEffect(() => {
-    axios
-      .get("https://hn.algolia.com/api/v1/search?query=react")
-      .then((res) => console.log(res.data));
+    const getData = async () => {
+      const data = await fetchArticles();
+
+      setArticles(data.hits);
+    };
+    getData();
   }, []);
   return (
     <div>
       <h2>HTTP</h2>
+      <ul>
+        {articles.map((item) => (
+          <li key={item.ObjectId}>
+            <a href={item.url} target="blank">
+              {item.title}
+            </a>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
 export default App;
+
+// axios.get("https://hn.algolia.com/api/v1/search?query=react").then((res) => console.log(res.data));
---
+++ src/services/api.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export const fetchArticles = async () => {
+  const { data } = await axios.get(
+    "https://hn.algolia.com/api/v1/search?query=react"
+  );
+  return data;
+};
