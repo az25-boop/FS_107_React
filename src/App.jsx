927f1c2c00627962e233343bd9e9f53a1ab5126e
Pavel_Melnik <129269794+az25-boop@users.noreply.github.com>
2024-09-18 11:36:28 +0300

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,11 @@
+import { useEffect } from "react";
+
 const App = () => {
+  useEffect(() => {
+    axios
+      .get("https://hn.algolia.com/api/v1/search?query=react")
+      .this((res) => console.log(res.data));
+  }, []);
   return (
     <div>
       <h2>HTTP</h2>
